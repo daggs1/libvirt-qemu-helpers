4b8d3df14956adf550c57e78617543a0520841f6
Dagg Stompler <daggs@gmx.com>
2023-05-16 20:13:56 +0300

    vm_utils: implement basic funcs
    implement vars init func that inits the base and needed vars
    and the init log func which inits the log
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- usr/local/bin/vm_utils.sh
+++ usr/local/bin/vm_utils.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+function init_base_vars {
+	local GST_NAME="$1"
+	local STATE="$2"
+	local STAGE="$3"
+	local LOGS_PATH="/var/log/libvirt/qemu/helpers"
+	local STATE_PATH="/var/lib/libvirt/helpers_state/${GST_NAME}"
+	local CFG_PATH="/etc/libvirt/hooks/qemu.d"
+	local curr_pid=$(ps aux | grep " start ${GST_NAME}" | grep -v grep | awk '{print $2}')
+	local EXEC_USER=$(ps -o user= -p ${curr_pid})
+
+	for var in {GST_NAME,STATE,STAGE,EXEC_USER,CFG_PATH,STATE_PATH,LOGS_PATH}; do
+		echo "${var}=$(eval echo \${${var}})"
+	done
+}
+
+function init_log {
+	local log_path="${LOGS_PATH}/${GST_NAME}.log"
+
+	mkdir -p $(dirname ${log_path})
+	exec 2> >(tee -a -i "${log_path}")
+	set -x
+	exec >> "${log_path}"
+	echo "log started at $(date)"
+}
+
