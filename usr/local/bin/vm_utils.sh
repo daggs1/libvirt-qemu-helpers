543e2216d7a2ed36016e7044a1c5ed29e5efa394
Dagg Stompler <daggs@gmx.com>
2023-05-17 20:56:02 +0300

    vm_utils: add pt pcie reader func
    add a func that reads the vm's xml file and extract from it the pci addr for
    all pcie devices which are configured as pass-through
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- usr/local/bin/vm_utils.sh
+++ usr/local/bin/vm_utils.sh
@@ -9,8 +9,22 @@ function init_base_vars {
 	local CFG_PATH="/etc/libvirt/hooks/qemu.d"
 	local curr_pid=$(ps aux | grep " start ${GST_NAME}" | grep -v grep | awk '{print $2}')
 	local EXEC_USER=$(ps -o user= -p ${curr_pid})
+	local VM_XML_FILE_PATH=
 
-	for var in {GST_NAME,STATE,STAGE,EXEC_USER,CFG_PATH,STATE_PATH,LOGS_PATH}; do
+	local xmls_path="${CFG_PATH}/../../qemu"
+	OIFS=${IFS}
+	IFS=$'\n'
+
+	for xml_file_path in $(find ${xmls_path} -name "*.xml" -type f); do
+		xmllint --xpath "//domain/name/text()" ${xml_file_path} 2>/dev/null | egrep -q "^${GST_NAME}$" || continue
+		VM_XML_FILE_PATH=${xml_file_path}
+		break
+	done
+
+	OIFS=${OIFS}
+
+	local VM_PCIE_PT_BFDS=$(export VM_PCIE_PT_BFDS; gather_pcie_pt_bdfs)
+	for var in {GST_NAME,STATE,STAGE,EXEC_USER,CFG_PATH,STATE_PATH,LOGS_PATH,VM_XML_FILE_PATH,VM_PCIE_PT_BFDS}; do
 		echo "${var}=$(eval echo \${${var}})"
 	done
 }
@@ -32,4 +46,18 @@ function prep_state {
 
 function release_state {
 	umount ${STATE_PATH}
+}
+
+function gather_pcie_pt_bdfs {
+	local bdfs=
+	local prefix="//domain/devices/hostdev[@type='pci']/source/address"
+	OIFS=${IFS}
+	IFS=","
+
+	for pcie in $(xmllint --xpath "${prefix}/@domain | ${prefix}/@bus | ${prefix}/@slot | ${prefix}/@function" ${VM_XML_FILE_PATH} 2>/dev/null | cut -f 2 -d = | sed 's/"//g;s/0x//g' | sed '0~4 a\,\'); do
+		bdfs="${bdfs},$(echo "${pcie}" | xargs | sed 's/ /./3;s/ /:/g')"
+	done
+
+	IFS=${OIFS}
+	echo ${bdfs:1}
 }
